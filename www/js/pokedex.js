31d55493ecd1c07a2a5e8956bb03025a6d12b9c7
david <davidjeff1721659462@gmail.com>
2020-05-16 11:05:41 -0500

    version1

---
+++ www/js/pokedex.js
@@ -0,0 +1,32 @@
+function consultar(){
+    nombre = $('#txtNombre').val();
+    dir = "https://pokeapi.co/api/v2/pokemon/"+nombre
+
+    $.ajax({
+        url:dir,
+        error:function(err){
+            alert("El Pokemon no fue encontrado");
+            console.log(err);
+
+            $("#divCargando").hide();
+        },
+        beforeSend:function(){
+            $("#pokeInfo").hide();
+            $("#divCargando").show();
+        }
+
+    }).done(function(data){
+        $("#divCargando").hide();
+        $("#pokeInfo").show();
+        console.log(data);
+        document.getElementById('fotoPoke').src = data.sprites.front_default;
+        document.getElementById('fotoPokeb').src = data.sprites.back_shiny;
+        $('#dataPeso').html(data.weight);
+        $('#dataname').html(data.name);
+    });  
+
+}
+
+
+
+
